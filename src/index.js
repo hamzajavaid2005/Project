ebb26417795c0417b21ea08ab3ae4344d431c1fa
Hamza Javaid <hamzajavaid2005@gmail.com>
2024-06-27 20:52:09 +0500

    Connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: "./env"
+});
+
+connectDB();
+
+
+// import express from 'express';
+
+// const app = express();
+
+// (async() => {
+//     try {
+//         await mongoose.connect(`${proscess.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR:", error);
+//             throw error;
+
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`Server is running on port ${process.env.PORT}`);
+
+//             })
+//         })
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// })
